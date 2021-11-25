f1c29458b0327e60b63ba746f36c07fd3f400974
Sheetal575 <sheetaldadh9@gmail.com>
2021-11-25 11:14:21 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import login from'./login-page/login'
+function App() {
+  return (
+    <div className="App text-center h-100">
+       <login />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/login-page/login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './login.css';
+
+export function login(){
+    return(
+        <h1>Login page</h1>
+    )
+}
