64e2e041b9865912add3ac9b36f6ae4f5e7b3fa3
Sheetal575 <sheetaldadh9@gmail.com>
2021-12-03 14:08:24 +0530

    view project page ui is created

--- src/components/Team-Members/Add-Team-Members/add-member.js
+++ src/components/Team-Members/Add-Team-Members/add-member.js
@@ -19,44 +19,45 @@ export default function AddTeamMember(){
             <br />
             <div className="row justify-content-between ">
                     <div className="col-lg-4 mt-3">
-                        <label for="emailaddress">Member Name</label>
+                        <label htmlFor="emailaddress">Member Name</label>
                         <input className="form-control mt-2" id="emailaddress" type="text" placeholder="Enter Name" />
                     </div>
                     
                     <div className="col-lg-3 mt-3">
-                        <label for="projectstatus">Select Year </label>
-                        <select class="form-select mt-2" id="projectstatus" >
-                            <option value="1">Web</option>
-                            <option value="1">Android</option>
-                            <option value="2">ML</option>
+                        <label htmlFor="projectstatus">Select Year </label>
+                        <select className="form-select mt-2" id="projectstatus" >
+                            <option value="1">First</option>
+                            <option value="1">Second</option>
+                            <option value="2">third</option>
+                            <option value="2">final</option>
                         </select>
                     </div>
                     <div className="col-lg-4 mt-3">
-                        <label for="emailaddress">Branch</label>
+                        <label htmlFor="emailaddress">Branch</label>
                         <input className="form-control mt-2" id="emailaddress" type="text" placeholder="Enter Branch" />
                     </div>
                     <div className="col-lg-3 mt-3">
-                        <label for="emailaddress">Linkedin Url</label>
+                        <label htmlFor="emailaddress">Linkedin Url</label>
                         <input className="form-control mt-2" id="emailaddress" type="text" placeholder="Enter Url" />
                     </div>
                     <div className="col-lg-3 mt-3">
-                        <label for="emailaddress">Github Url</label>
+                        <label htmlFor="emailaddress">Github Url</label>
                         <input className="form-control mt-2" id="emailaddress" type="text" placeholder="Enter Url" />
                     </div>
                     <div className="col-lg-3 mt-3">
-                        <label for="emailaddress">Instagram Url</label>
+                        <label htmlFor="emailaddress">Instagram Url</label>
                         <input className="form-control mt-2" id="emailaddress" type="text" placeholder="Enter Url" />
                     </div>
                     <div className="col-lg-2 mt-3">
-                        <label for="projectstatus">Select Role </label>
-                        <select class="form-select mt-2" id="projectstatus" >
+                        <label htmlFor="projectstatus">Select Role </label>
+                        <select className="form-select mt-2" id="projectstatus" >
                             <option value="1">Lead</option>
                             <option value="1">Web Lead</option>
                             <option value="2">ML</option>
                         </select>
                     </div>
                     <div className="mt-3 col-lg-4">
-                        <label for="memberphoto" >Upload member's photo</label>
+                        <label htmlFor="memberphoto" >Upload member's photo</label>
                         <input type="file" className="form-control mt-2" onChange={handleChange} />
                     </div>
                     <div className="mt-5 col-lg-7 ">
@@ -72,7 +73,7 @@ export default function AddTeamMember(){
                     
             </div>
             <div className="row text-center mt-5 justify-content-center">
-                    <button type="button" class="btn create-project-button">Add </button>
+                    <button type="button" class="btn primary-button">Add </button>
             </div>
         </div>
     )
--- src/components/Team-Members/View-Team-Members/view-members.js
+++ src/components/Team-Members/View-Team-Members/view-members.js
@@ -1,7 +1,81 @@
 import React from "react";
+import './view-members.css';
 
 export default function ViewTeamMembers(){
     return(
-        <div>view team member works</div>
+        <>
+        <div className="row mt-2">
+            <button className="btn primary-button add-member">Add Member</button>
+        </div>
+        <br />
+        <div className="table-container p-0">
+            <table>
+                <thead className="table-header">
+                    <tr>
+                        <th> Name</th>
+                        <th>Role</th>
+                        <th>Branch</th>
+                        <th>Year</th>
+                        <th>Linkedin Id</th>
+                        <th>Github Id</th>
+                        <th>Intagram Id</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody className="table-body">
+                <tr>
+                    <td>Sheetal</td>
+                    <td>Web Lead</td>
+                    <td>Computer Science and Engineering</td>
+                    <td>3rd year</td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Sheetal</td>
+                    <td>Web Lead</td>
+                    <td>Computer Science and Engineering</td>
+                    <td>3rd year</td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                </tr>
+                <tr>
+                    <td>Sheetal</td>
+                    <td>Web Lead</td>
+                    <td>Computer Science and Engineering</td>
+                    <td>3rd year</td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Sheetal</td>
+                    <td>Web Lead</td>
+                    <td>Computer Science and Engineering</td>
+                    <td>3rd year</td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td><a href="https://www.linkedin.com/in/sheetal-dadhich-a48980191/">Link</a></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        </>
     )
 }
