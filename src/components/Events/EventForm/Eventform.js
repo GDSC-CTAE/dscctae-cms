f26034b31af7403460ce238de396f0648ed8f54f
Ishika Tiwari <ishikatiwari416@gmail.com>
2021-12-12 18:35:13 +0530

    Added image section, submit button in the form for creating event

--- src/components/Events/EventForm/Eventform.js
+++ src/components/Events/EventForm/Eventform.js
@@ -4,8 +4,13 @@ import "react-datepicker/dist/react-datepicker.css"
 
 
 export default function EventForm(){
+    const [image , setimage] = useState(null);
 
-    
+    const handleChange = (event) => {
+      
+         setimage(URL.createObjectURL(event.target.files[0]))
+         
+      }
        
             const [startDate, setStartDate] = useState(null);
             const showCalendar = () => setStartDate();
@@ -14,36 +19,60 @@ export default function EventForm(){
     
     return(
         <>        
-               <div className="card">
-                   <div className="row m-2 w-100 justify-content-around">
+               <div className="card p-5">
+                   <div className="row justify-content-between">
 
-                        <div className="form-group col-sm-3 col-md-3 col-lg-3 col-xl-3 ">
+                       <div className="m-2">
+                           <h5 className="text-muted">Create Event</h5>
+                       </div>
+
+                        <div className="form-group mt-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 ">
                                 <label for="exampleFormControlInput1" className="m-1">Name of event</label> 
-                                       <input className="form-control " id="exampleFormControlInput1" type="text"/>
+                                       <input className="form-control " placeholder="Event Name" id="exampleFormControlInput1" type="text"/>
                         </div>
 
 
-                        <div className="form-group col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                        <div className="form-group mt-4 col-sm-3 col-md-3 col-lg-3 col-xl-3">
                                 <label for="exampleFormControlInput1" className="m-1">Date of event</label> 
                                 <DatePicker selected ={startDate} onChange={ (Date) => setStartDate(Date)}
                                 className="form-control" id="exampleFormControlInput1"
-                                />
-                             
-                                
-                                
+                                />        
                         </div>
 
-                        <div className="col-sm-3 col-md-3 col-lg-3 col-xl-3">
+                        <div className="form-group mt-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
                             <label for="exampleFormControlInput1" className="m-1">Organiser</label>
-                            <input className="form-control" id="exampleFormControlInput1" type="text" />
+                            <input className="form-control" placeholder="Organiser's Name" id="exampleFormControlInput1" type="text" />
                         </div>
 
-                        <div className="form-group m-2 p-2 col-sm-11 col-md-11.5 col-lg-10.3 col-xl-10.5">
-                           <label for="exampleFormControlTextarea1">Description</label>
-                           <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>   
+                        <div className="form-group mt-2 col-12">
+                           <label for="exampleFormControlTextarea1" className="m-1">Description</label>
+                           <textarea className="form-control" placeholder="Write a description of the event" id="exampleFormControlTextarea1" rows="5"></textarea>   
                        </div>
-                       <input type="file" />
+
+                       
+                       <div className=" mt-2">
+                           <input className="form-control" type="file" onChange={handleChange} />     
+                       </div>
+
+                       <div className="row mt-3 justify-content-center">
+                           {
+                               image? (
+                                    <div className="col-6">
+                                         <img src={image} className="w-100 h-100 "></img>
+                                    </div>
+                               ) : (
+                                   null
+                               )
+                           }
+                          
+                       </div>
+                      
                     </div>
+
+                     
+                    <div className="row m-2 justify-content-center">
+                         <button type="button" className="btn btn-primary w-auto">Submit</button>
+                      </div>
 
                  </div>   
 
