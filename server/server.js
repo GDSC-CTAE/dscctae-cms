14ca118b1e4d66711f01ace88a4b7980730c31d5
Ishika Tiwari <ishikatiwari416@gmail.com>
2021-12-15 01:47:23 +0530

    Added search bar

--- server/server.js
+++ server/server.js
@@ -6,6 +6,23 @@ app.get("/", (req, res) => {
   res.send("Hello Developers");
 });
 
+const addeventhandle = (eventdata) => {
+  console.log(eventdata)
+  db.collection("Events")
+    .set(eventdata)
+    .then( (response) => {
+      console.log("Event Added Successfully")
+    })
+}
+
+app.post("/newevent" , (req, res) => {
+  console.log(req.body)
+  addeventhandle(req.body)
+  res.send()
+ }     
+)
+
 app.listen(5000, () => {
   console.log("Your Server is Created and Running Successfully!");
 });
+
--- src/components/Events/events.js
+++ src/components/Events/events.js
@@ -2,37 +2,54 @@ import React from "react"
 import Eventform from "./EventForm/Eventform.js";
 import { EventInfo } from "./EventData/eventdata.js";
 import {Link} from 'react-router-dom'
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
+//import ReactPaginate from 'react-Paginate'
 
 export default function Events(){
-    
+
+    const [searchquery, setSearchQuery] = useState("")
+    const [pageCount, setPageCount] = useState(0);
 
     return(
-
         <>
-
             <div>
+                        <div className="p-2 m-2 col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-5">
+                                <input onChange={ e => setSearchQuery(e.target.value) } className="form-control" placeholder="Search by event name"></input>
+                        </div>
+
                 {EventInfo.map( (val, index) => {
                     return(
-                        <center>
+                        <center>           
+
                         <div className="card p-2 m-3 " >
                            
-                            <div className="row  align-items-center" key={index} >
-                            <div className="p-2 text-start col-sm-6 col-md-6 col-lg-6 col-xl-6">
-                                    <h5> {val.nameofevent} </h5>
-                                    <h5> {val.organiser} </h5>
-                                    <h5> {val.dateofevent} </h5>
+                            <div className="row align-items-center" key={index} >
+                               <div className="p-2 text-start col-sm-8 col-md-8 col-lg-8 col-xl-8">
+                                    <h3> {val.nameofevent} </h3>
+                                    <h4 className="text-muted"> {val.organiser} </h4>
+                                    <h6> {val.dateofevent} </h6>
+                                </div>
+
+                            <div className="p-2 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+                                 <Link to="/Eventform" className="m-2 btn btn-primary">View event</Link>
+                                 <Link to="/Eventform" className="m-2 btn btn-primary">Edit event</Link>
                             </div>
 
-                            <div className="p-2 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                                 <Link to="/Eventform" className="btn btn-primary">View event</Link>
-                            </div>
-
-                            <div className="p-2 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                                 <Link to="/Eventform" className="btn btn-primary">Edit event</Link>
-                            </div>
                             </div>
                            
                         </div>
+
+                        {/* <ReactPaginate
+                             breakLabel="..."
+                             nextLabel="next >"
+                             onPageChange={handlePageClick}
+                             pageRangeDisplayed={5}
+                             pageCount={pageCount}
+                             previousLabel="< previous"
+                             renderOnZeroPageCount={null}>
+                        </ReactPaginate> */}
+
                         </center>
                     )
                  })}
